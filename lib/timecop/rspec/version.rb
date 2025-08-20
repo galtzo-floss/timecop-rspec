daf2f1f75d3ae9e618f1b1f9dfed91d1950cf75c
Peter H. Boling <peter.boling@gmail.com>
2025-08-20 06:32:21 -0600

    🔖 Prepare release v1.0.3
    ⚡️ A message from a fellow meat-based-AI ⚡️
    - [❤️] Finely-crafted open-source tools like timecop-rspec (& many more) are a full-time endeavor.
    - [❤️] Though I adore my work, it lacks financial sustainability.
    - [❤️] Please, help me continue enhancing your tools by becoming a sponsor:
      - [💲] https://liberapay.com/pboling/donate
      - [💲] https://github.com/sponsors/pboling

--- lib/timecop/rspec/version.rb
+++ lib/timecop/rspec/version.rb
@@ -4,7 +4,7 @@ class Timecop
     module Version
       # The gem version.
       # @return [String]
-      VERSION = "1.0.2"
+      VERSION = "1.0.3"
     end
     # Convenience constant aliasing Version::VERSION
     # @return [String]
